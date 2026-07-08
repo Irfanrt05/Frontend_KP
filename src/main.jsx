1f7e50254c367d019fe290f3c77adfe41d5c0988
Muhammad Irfan Hakim <irfanhakim.4320@gmail.com>
2026-07-09 00:31:14 +0700

    Update Google OAuth Client ID

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,8 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GoogleOAuthProvider
-      clientId="471473422571-dumsu4tmj0gbgov3kl9g8dd232h0t5dt.apps.googleusercontent.com"
-    >
+    <GoogleOAuthProvider clientId="465744673575-ikjdlstm0audsifp3q47i0jot2d3apt5.apps.googleusercontent.com">
       <App />
     </GoogleOAuthProvider>
-  </StrictMode>
+  </StrictMode>,
 );
